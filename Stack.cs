1aea42493be39ef4c93d0b94dda83f0122d4763f
Andres <floresc.andres@intracitygeeks.org>
2018-11-01 21:42:47 -0400

    criteria builder

---
+++ Stack.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stacker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+            stack.GetElement();
+        }
+    }
+
+    class Stack
+    {
+        public void GetElement()
+        {
+            Console.Write("");
+        }
+        void Push(object obj)
+        {
+            
+        }
+
+        private object Pop()
+        {
+            Console.WriteLine();
+        }
+
+        void Clear()
+        {
+
+        }
+    }
+}
