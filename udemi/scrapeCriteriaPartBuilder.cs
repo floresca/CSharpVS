9cbf28946fdc0865e115c910926af2a483376949
Andres <floresc.andres@intracitygeeks.org>
2018-11-02 18:03:48 -0400

    Part builder

---
+++ udemi/scrapeCriteriaPartBuilder.cs
@@ -0,0 +1,48 @@
+using SimpleScrape.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SimpleScrape.Builders
+{
+    class ScrapeCriteriaPartBuilder
+    {
+
+        public string _regex;
+        public RegexOptions _regexOption;
+
+        public ScrapeCriteriaPartBuilder()
+        {
+            SetDefaults();
+        }
+
+        private void SetDefaults()
+        {
+            _regex = string.Empty;
+            _regexOption = RegexOptions.None;
+        }
+
+        public ScrapeCriteriaPartBuilder WithRegex(string regex)
+        {
+            _regex = regex;
+            return this;
+        }
+
+        public ScrapeCriteriaPartBuilder WithRegexOption(RegexOptions regexOption)
+        {
+            _regexOption = regexOption;
+            return this;
+        }
+
+        public ScrapeCriteriaPart Build()
+        {
+            ScrapeCriteriaPart scrapeCriteriaPart = new ScrapeCriteriaPart();
+            scrapeCriteriaPart.Regex = _regex;
+            scrapeCriteriaPart.RegexOption = _regexOption;
+            return scrapeCriteriaPart;
+        }
+    }
+}
