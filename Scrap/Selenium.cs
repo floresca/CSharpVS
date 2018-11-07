db4f73df6b619bd592c7d8c08e5d11f739970ea7
Andres <floresc.andres@intracitygeeks.org>
2018-11-06 23:31:52 -0500

    few comments

--- Scrap/Selenium.cs
+++ Scrap/Selenium.cs
@@ -1,6 +1,6 @@
-using NUnit.Framework;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
+// using NUnit.Framework;
+// using OpenQA.Selenium;
+// using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
 using System.Linq;
