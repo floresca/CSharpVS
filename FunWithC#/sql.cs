5674984021d45b8540e16ef8d553b1fdaade928d
Andres <floresc.andres@intracitygeeks.org>
2018-11-19 20:23:36 -0500

    Sharpening a DB connect

---
+++ FunWithC#/sql.cs
@@ -0,0 +1,5 @@
+SqlConnection myConnection = new SqlConnection("user id=username;" + 
+                                       "password=password;server=serverurl;" + 
+                                       "Trusted_Connection=yes;" + 
+                                       "database=database; " + 
+                                       "connection timeout=30");
