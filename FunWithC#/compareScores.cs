09a4d55e1ca5b55b27e74391a5e04e2fa3d42132
Andres <floresc.andres@intracitygeeks.org>
2018-12-03 19:13:23 -0500

    Compared numbers, can't convert list int to int

--- FunWithC#/compareScores.cs
+++ FunWithC#/compareScores.cs
@@ -1,5 +1,21 @@
-static List<int> compareTriplets(List<int> a, List<int> b) {
-        
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {   
+
+    // Complete the compareTriplets function below.
+    static List<int> compareTriplets(List<int> a, List<int> b) {
         int scoreA = 0;
         int scoreB = 0;
         
@@ -15,10 +31,25 @@ static List<int> compareTriplets(List<int> a, List<int> b) {
             }
         }
         
-        int[] scores = new int[2];
-        scores[0] = scoreA;
-        scores[1] = scoreB;
+        List<int> scores = new List<int>() ;
+        scores.Add(scoreA);
+        scores.Add(scoreB);
 
-        return scores[];
-        
+        return scores;
     }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+        List<int> b = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(bTemp => Convert.ToInt32(bTemp)).ToList();
+
+        List<int> result = compareTriplets(a, b);
+
+        textWriter.WriteLine(String.Join(" ", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
