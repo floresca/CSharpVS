3693d5fc64326e2781438b4cdd38ff6fa750f994
Andres <floresc.andres@intracitygeeks.org>
2018-12-29 18:16:34 -0500

    absolute diagonals

---
+++ FunWithC#/AbsoluteDiagonals.cs
@@ -0,0 +1,50 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the diagonalDifference function below.
+    static int diagonalDifference(int[][] arr) {
+    
+    int sum = 0;
+    int n = arr.Length;
+
+    for (var i = 0; i < n; i++){
+        sum += arr[i][i];
+        sum -= arr[i][n-1-i];
+    }
+
+    return Math.Abs(sum);
+
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[][] arr = new int[n][];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+        }
+
+        int result = diagonalDifference(arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
