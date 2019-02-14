58c79cbe81089ec0362b42e78a6a12a1325455c5
Andres <floresc.andres@intracitygeeks.org>
2019-02-13 23:36:49 -0500

    token move

--- FunWithC#/DCC-2-TicTacToeBoard.cs
+++ FunWithC#/DCC-2-TicTacToeBoard.cs
@@ -508,6 +508,7 @@ namespace DCC_2_TicTacToe
                     location = 8;
                 } 
                 //play game with whatever location is
+                TokenMove();
             }
         }
         
